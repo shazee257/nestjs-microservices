37ed88876ac1bedc5f2bbb0017888f9f919eea62
Shahzad <shazee257@gmail.com>
2024-08-06 20:21:49 +0500

    first commit

---
+++ analytics/src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { EventPattern } from '@nestjs/microservices';
+import { CreateUserEvent } from './create-user.event';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @EventPattern('user_created')
+  handleUserCreated(data: CreateUserEvent) {
+    console.log('analytics controller >>>');
+    return this.appService.handleUserCreated(data);
+  }
+}
---
+++ analytics/src/app.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserEvent } from './create-user.event';
+
+@Injectable()
+export class AppService {
+  private readonly analytics: any[] = [];
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  handleUserCreated(data: CreateUserEvent) {
+    console.log('User created:', data);
+    this.analytics.push({
+      email: data.email,
+      created_at: new Date(),
+    });
+
+    return this.analytics;
+  }
+}
